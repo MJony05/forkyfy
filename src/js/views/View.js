3b409c866048b755bdb23f702bac4c3b484796b1
Jonibek <munirovjonibek@gmail.com>
2022-04-21 23:12:48 +0500

    finished dynamic serving

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -18,9 +18,25 @@ export default class View {
     const newDOM = document.createRange().createContextualFragment(newMarkup);
 
     const newElements = Array.from(newDOM.querySelectorAll('*'));
-    console.log(newElements);
+    // console.log(newElements);
     const curElements = Array.from(this._parentElement.querySelectorAll('*'));
-    console.log(curElements);
+    // console.log(curElements);
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      // console.log(curEl, newEl.isEqualNode(curEl));
+
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        curEl.textContent = newEl.textContent;
+      }
+      if (!newEl.isEqualNode(curEl)) {
+        Array.from(newEl.attributes).forEach(attr => {
+          curEl.setAttribute(attr.name, attr.value);
+        });
+      }
+    });
   }
   _clear() {
     this._parentElement.innerHTML = '';
